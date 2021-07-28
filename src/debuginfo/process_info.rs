278c40e9ed506c35bbeba107f6257d8d76840944
cx <simon.farre.cx@gmail.com>
2021-07-28 21:55:29 +0200

    cleaned up and added windows conditional compilation here so that this can compile on windows (however it won't provide the feature in that case)

--- src/debuginfo/process_info.rs
+++ src/debuginfo/process_info.rs
@@ -17,9 +17,10 @@ pub struct ProcessInfo {
 }
 
 impl ProcessInfo {
+    #[cfg(target_os = "linux")]
     pub fn new() -> std::io::Result<ProcessInfo> {
         let rss = {
-            let mut file = std::fs::File::open("/proc/self/smaps_rollup").expect("failed to open smaps_rollup");
+            let mut file = std::fs::File::open("/proc/self/smaps_rollup")?;
             let mut buf = String::with_capacity(1024);
             file.read_to_string(&mut buf)?;
 
@@ -32,7 +33,7 @@ impl ProcessInfo {
             data.parse().unwrap()
         };
 
-        let mut file = std::fs::File::open("/proc/self/status").expect("failed to open status");
+        let mut file = std::fs::File::open("/proc/self/status")?;
         let mut buf = String::with_capacity(1024);
         file.read_to_string(&mut buf)?; // .expect("failed to read data");
         let to_find = vec![0, 5, 16, 17, 28];
@@ -43,13 +44,22 @@ impl ProcessInfo {
             .map(|(i, line)| line.chars().filter(|c| if i == 0 { true } else { c.is_digit(10) }).collect())
             .collect();
         let name = items.remove(0).chars().skip(6).collect();
+
+        // We either get a value, or we map the Error returned from .parse() into an std::io::Error (otherwise they return different types)
+        let pid = items.remove(0).parse().map(|v| v).map_err(|_| std::io::ErrorKind::InvalidInput)?;
         Ok(ProcessInfo {
             name,
-            pid: items.remove(0).parse().expect("failed to parse pid"),
+            pid,
             virtual_mem_usage_peak: items.remove(0).parse().expect("failed to parse peak virtual memory usage"),
             virtual_mem_usage: items.remove(0).parse().expect("failed to parse virtual memory usage"),
             rss,
             shared_lib_code: items.remove(0).parse().expect("failed to parse shared library code size"),
         })
+    }
+
+    #[cfg(target_os = "windows")]
+    pub fn new() -> std::io::Result<ProcessInfo> {
+        println!("This is not yet implemented for windows. Will just show 0's");
+        Ok(ProcessInfo { name: 0, pid: 0, virtual_mem_usage_peak: 0, virtual_mem_usage: 0, rss: 0, shared_lib_code: 0 })
     }
 }
