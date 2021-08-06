51495255085f8a636a49633a5cb448f9c7466444
cx <simon.farre.cx@gmail.com>
2021-08-06 20:46:02 +0200

    cleaned up rectangle shader

--- src/assets/rectangle.fs.glsl
+++ src/assets/rectangle.fs.glsl
@@ -1,11 +1,5 @@
 #version 430 core
 out vec4 FragColor;
-// The RGB Color
-// in vec4 rect_color;
-// The interpolation value; decides if we use the bound texture
-// in float interpolation;
-/// size of the rectangle currently getting drawn
-// in vec2 u_size;
 // from http://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
 float box_signed_distance_field_rounding(vec2 CenterPosition, vec2 Size, float Radius) {
     return length(max(abs(CenterPosition)-Size+Radius,0.0))-Radius;
@@ -15,6 +9,7 @@ uniform float radius;
 uniform vec2 rect_pos;
 uniform sampler2D texture_sampler;
 
+// *much* cleaner now.
 in RectangleInfo {
     vec2 tex_coords;
     vec2 size;
@@ -36,16 +31,13 @@ void main()
     
     if(radius > 0.0) {
         // The pixel space scale of the rectangle.       
-        // the pixel space location of the rectangle.
         vec2 location = rect_pos;
         float cutoff = location.y + rectangleInfo.size.y / 2.0;
         float edgeSoftness  = 0.1f;
-        // Calculate distance to edge.
-        
+        // the actual magic behind signed distance fields/functions
         float dist = box_signed_distance_field_rounding(gl_FragCoord.xy - location - (rectangleInfo.size/2.0f), rectangleInfo.size / 2.0f, radius);
         float smoothedAlpha =  1.0 - smoothstep(0.0f, edgeSoftness * 2.0 ,dist);
-            // This will be our resulting "shape". 
-        // vec4 quadColor = mix(vec4(0.0, 0.0, 0.0, 0.0), chosen_color, smoothedAlpha);
+        // This will be our resulting "shape". 
         vec4 quadColor = mix(vec4(0.0, 0.0, 0.0, 0.0), vec4(chosen_color.rgb, smoothedAlpha), smoothedAlpha);
         FragColor = quadColor;
     } else {
--- src/assets/rectangle.vs.glsl
+++ src/assets/rectangle.vs.glsl
@@ -1,15 +1,13 @@
 #version 430 core
 layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 uv>
-layout (location = 1) in vec4 color;  // <vec3 col, float interpolate>
+layout (location = 1) in vec4 color;  // color2
 
+// todo: move this into a uniform buffer object, because this uniform
+// will be the same across all shader programs
 uniform mat4 projection;
+
 uniform vec2 rect_size;
 uniform float useTexture;
-
-// out vec4 rect_color;    // RGB color 
-// out vec2 u_size;        // size of window
-// out vec2 texture_coordinates;
-// out vec2 texel_coords;  // texel (fragment) which we reference in the fragment shader, to calculate SDF, inside box
 
 out RectangleInfo {
     vec2 tex_coords;
@@ -21,11 +19,7 @@ out RectangleInfo {
 void main()
 {    
     gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
-    // rect_color = color;
-    // texture_coordinates = vertex.zw;
-    // texel_coords = vertex.xy;
-    // u_size = rect_size;
-
+    // *really* clean and nice shader now
     rectangleInfo.tex_coords = vertex.zw;
     rectangleInfo.color = color;
     rectangleInfo.size = rect_size;
