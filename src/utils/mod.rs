4ff29b5bfa99b7607618bf20f102fbb68db5e11d
cx <simon.farre.cx@gmail.com>
2021-08-10 22:28:12 +0200

    renamed the memcpy we use, to signal that it must *not* be overlapping

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,7 +5,7 @@ pub mod macros;
 
 /// Copies slice to memory pointed at by dst.
 #[inline(always)]
-pub unsafe fn copy_slice_to<T>(dst: *mut T, slice: &[T]) {
+pub unsafe fn non_overlap_copy_slice_to<T>(dst: *mut T, slice: &[T]) {
     std::ptr::copy_nonoverlapping(slice.as_ptr(), dst, slice.len());
 }
 
